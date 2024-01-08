719c8c45315f97306cdcc8d7a4eac7c8486ecd4b
lilPlumberBoy <dalymike102@gmail.com>
2024-01-08 16:12:12 -0500

    fix: fixed linter config

---
+++ script/Deployments.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import { DeployScript } from "forge-deploy/DeployScript.sol";
+// generated from looking at contracts with ./forge-deploy gen-deployer
+import {
+    DeployerFunctions,
+    DefaultDeployerFunction,
+    Deployer,
+    DeployOptions
+} from "generated/deployer/DeployerFunctions.g.sol";
+// Could also import the default deployer functions
+// import "forge-deploy/DefaultDeployerFunction.sol";
+
+contract Deployments is DeployScript {
+    // Using generated functions
+    using DeployerFunctions for Deployer;
+    // Using default deployer function
+    using DefaultDeployerFunction for Deployer;
+
+    DeployOptions public options;
+
+    function deploy() public { }
+}
+// example run in current setup: DEPLOYMENT_CONTEXT=localhost forge script script/Deployments.s.sol --rpc-url
+// http://localhost:8545 --broadcast --private-key ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -v
+// && ./forge-deploy sync;
