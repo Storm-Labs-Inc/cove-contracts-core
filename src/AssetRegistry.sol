b72545e05fa0541db4a62c490dd92b3a8c24f66d
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2024-07-01 13:55:31 -0400

    feat: add asset registry

---
+++ src/AssetRegistry.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.23;
+
+import { AccessControlEnumerable } from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
+
+import { Errors } from "src/libraries/Errors.sol";
+
+/**
+ * @title AssetRegistry
+ * @dev Manages the registration and status of assets in the system.
+ * @notice This contract provides functionality to add and pause assets, with role-based access control.
+ */
+contract AssetRegistry is AccessControlEnumerable {
+    /**
+     * Structs
+     */
+    struct AssetStatus {
+        /// @dev Indicates whether the asset is enabled in the registry.
+        bool enabled;
+        /// @dev Indicates whether the asset is currently paused.
+        bool paused;
+    }
+
+    /**
+     * Constants
+     */
+    /// @notice Role responsible for managing assets in the registry.
+    bytes32 private constant _MANAGER_ROLE = keccak256("MANAGER_ROLE");
+
+    /**
+     * State variables
+     */
+    // slither-disable-next-line uninitialized-state
+    /// @dev Mapping from asset address to its status in the registry.
+    mapping(address => AssetStatus) private _assetRegistry;
+
+    /**
+     * Events
+     */
+    /// @dev Emitted when a new asset is added to the registry.
+    event AddAsset(address indexed asset);
+
+    /// @dev Emitted when an asset is paused in the registry.
+    event PauseAsset(address indexed asset);
+
+    /**
+     * Errors
+     */
+    /// @notice Thrown when attempting to add an asset that is already enabled in the registry.
+    error AssetAlreadyEnabled();
+    /// @notice Thrown when attempting to perform an operation on an asset that is not enabled in the registry.
+    error AssetNotEnabled();
+    /// @notice Thrown when attempting to pause an asset that is already in a paused state.
+    error AssetAlreadyPaused();
+
+    /**
+     * @notice Initializes the AssetRegistry contract
+     * @dev Sets up initial roles for admin and manager
+     * @param admin The address to be granted the DEFAULT_ADMIN_ROLE
+     */
+    // slither-disable-next-line locked-ether
+    constructor(address admin) payable {
+        if (admin == address(0)) revert Errors.ZeroAddress();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(_MANAGER_ROLE, msg.sender);
+    }
+
+    /**
+     * @notice Adds a new asset to the registry
+     * @dev Only callable by accounts with the MANAGER_ROLE
+     * @param asset The address of the asset to be added
+     */
+    function addAsset(address asset) external onlyRole(_MANAGER_ROLE) {
+        if (asset == address(0)) revert Errors.ZeroAddress();
+        if (_assetRegistry[asset].enabled) revert AssetAlreadyEnabled();
+
+        _assetRegistry[asset] = AssetStatus({ enabled: true, paused: false });
+        emit AddAsset(asset);
+    }
+
+    /**
+     * @notice Pauses an asset in the registry
+     * @dev Only callable by accounts with the MANAGER_ROLE
+     * @param asset The address of the asset to be paused
+     */
+    function pauseAsset(address asset) external onlyRole(_MANAGER_ROLE) {
+        if (asset == address(0)) revert Errors.ZeroAddress();
+        AssetStatus storage status = _assetRegistry[asset];
+        if (!status.enabled) revert AssetNotEnabled();
+        if (status.paused) revert AssetAlreadyPaused();
+
+        status.paused = true;
+        emit PauseAsset(asset);
+    }
+
+    /**
+     * @notice Retrieves the status of an asset
+     * @dev Returns default values (false, false) for non-existent assets
+     * @param asset The address of the asset to query
+     * @return enabled Whether the asset is enabled in the registry
+     * @return paused Whether the asset is currently paused
+     */
+    function getAssetStatus(address asset) external view returns (bool enabled, bool paused) {
+        AssetStatus memory status = _assetRegistry[asset];
+        return (status.enabled, status.paused);
+    }
+}
---
+++ test/unit/AssetRegistry.t.sol
@@ -0,0 +1,206 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.23;
+
+import { AssetRegistry } from "src/AssetRegistry.sol";
+import { Errors } from "src/libraries/Errors.sol";
+import { BaseTest } from "test/utils/BaseTest.t.sol";
+
+contract AssetRegistry_Test is BaseTest {
+    AssetRegistry public assetRegistry;
+    bytes32 public adminRole;
+    bytes32 public managerRole;
+
+    function setUp() public override {
+        super.setUp();
+        createUser("admin");
+        createUser("alice");
+        vm.startPrank(users["admin"]);
+        assetRegistry = new AssetRegistry(users["admin"]);
+        adminRole = assetRegistry.DEFAULT_ADMIN_ROLE();
+        managerRole = keccak256("MANAGER_ROLE");
+    }
+
+    function test_init() public view {
+        assert(assetRegistry.hasRole(adminRole, users["admin"]));
+        assert(assetRegistry.hasRole(managerRole, users["admin"]));
+    }
+
+    function test_constructor_revertWhen_zeroAddressAdmin() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        assetRegistry = new AssetRegistry(address(0));
+    }
+
+    // Try granting manager role from an account without admin role
+    function test_grantRole_revertWhen_CalledByNonAdmin() public {
+        vm.stopPrank();
+        vm.startPrank(users["alice"]);
+        // account is users["alice"]'s address, role is bytes(0) as defined in the contract
+        // TODO: fix issue with format of access control error
+        // vm.expectRevert(_formatAccessControlError(users["alice"], adminRole));
+        vm.expectRevert();
+        assetRegistry.grantRole(managerRole, users["alice"]);
+    }
+
+    // Try granting manager role from an account with admin role
+    function test_grantRole_managerRole() public {
+        // Check the user does not have the manager role
+        assert(!assetRegistry.hasRole(managerRole, users["alice"]));
+
+        // Grant the manager role to the user from the owner
+        assetRegistry.grantRole(managerRole, users["alice"]);
+
+        // Check the user now has the manager role
+        assert(assetRegistry.hasRole(managerRole, users["alice"]));
+    }
+
+    function test_grantRole_adminRole() public {
+        // Check the user does not have the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Grant the admin role to the user from the owner
+        assetRegistry.grantRole(adminRole, users["alice"]);
+
+        // Check the user now has the admin role
+        assert(assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Verify the user can grant the manager role
+        vm.stopPrank();
+        vm.prank(users["alice"]);
+        assetRegistry.grantRole(managerRole, users["bob"]);
+    }
+
+    function test_revokeRole_managerRole_revertWhen_RevokeRoleWithoutAdmin() public {
+        vm.stopPrank();
+        vm.startPrank(users["alice"]);
+        // account is users["alice"]'s address, role is bytes(0) as defined in the contract
+        // TODO: fix issue with format of access control error
+        // vm.expectRevert(_formatAccessControlError(users["alice"], adminRole));
+        vm.expectRevert();
+        assetRegistry.revokeRole(managerRole, users["admin"]);
+        vm.stopPrank();
+    }
+
+    function test_revokeRole_adminRole() public {
+        // Check the user does not have the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Grant the admin role to the user from the owner
+        assetRegistry.grantRole(adminRole, users["alice"]);
+
+        // Check the user now has the admin role
+        assert(assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Revoke the admin role from the user from the owner
+        assetRegistry.revokeRole(adminRole, users["alice"]);
+
+        // Check the user no longer has the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["alice"]));
+    }
+
+    function test_revokeRoleF_adminRole() public {
+        // Check the user does not have the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Grant the admin role to the user from the owner
+
+        assetRegistry.grantRole(adminRole, users["alice"]);
+
+        // Check the user now has the admin role
+        assert(assetRegistry.hasRole(adminRole, users["alice"]));
+
+        // Revoke the admin role from the user from the owner
+        vm.stopPrank();
+        vm.prank(users["alice"]);
+        assetRegistry.revokeRole(adminRole, users["alice"]);
+
+        // Check the user no longer has the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["alice"]));
+    }
+
+    function test_renounceRole_managerRole() public {
+        // Check the admin has the manager role
+        assert(assetRegistry.hasRole(managerRole, users["admin"]));
+
+        // Renounce the manager role from the admin
+        assetRegistry.renounceRole(managerRole, users["admin"]);
+
+        // Check the user no longer has the manager role
+        assert(!assetRegistry.hasRole(managerRole, users["admin"]));
+    }
+
+    function test_renounceRole_adminRole() public {
+        // Check the user has the admin role
+        assert(assetRegistry.hasRole(adminRole, users["admin"]));
+
+        // Renounce the admin role from the admin
+        assetRegistry.renounceRole(adminRole, users["admin"]);
+
+        // Check the user no longer has the admin role
+        assert(!assetRegistry.hasRole(adminRole, users["admin"]));
+    }
+
+    function test_addAsset_revertWhen_zeroAddress() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        assetRegistry.addAsset(address(0));
+    }
+
+    function testFuzz_addAsset_revertWhen_alreadyEnabled(address asset) public {
+        vm.assume(asset != address(0));
+
+        vm.expectEmit();
+        emit AssetRegistry.AddAsset(asset);
+        assetRegistry.addAsset(asset);
+
+        vm.expectRevert(AssetRegistry.AssetAlreadyEnabled.selector);
+        assetRegistry.addAsset(asset);
+    }
+
+    function testFuzz_addAsset(address asset) public {
+        vm.assume(asset != address(0));
+
+        vm.expectEmit();
+        emit AssetRegistry.AddAsset(asset);
+        assetRegistry.addAsset(asset);
+
+        (bool enabled, bool paused) = assetRegistry.getAssetStatus(asset);
+        assertTrue(enabled);
+        assertFalse(paused);
+    }
+
+    function test_pauseAsset_revertWhen_zeroAddress() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+        assetRegistry.pauseAsset(address(0));
+    }
+
+    function testFuzz_pauseAsset_revertWhen_notEnabled(address asset) public {
+        vm.assume(asset != address(0));
+
+        vm.expectRevert(AssetRegistry.AssetNotEnabled.selector);
+        assetRegistry.pauseAsset(asset);
+    }
+
+    function testFuzz_pauseAsset_revertWhen_alreadyPaused(address asset) public {
+        vm.assume(asset != address(0));
+
+        assetRegistry.addAsset(asset);
+        assetRegistry.pauseAsset(asset);
+
+        vm.expectRevert(AssetRegistry.AssetAlreadyPaused.selector);
+        assetRegistry.pauseAsset(asset);
+    }
+
+    function testFuzz_pauseAsset(address asset) public {
+        vm.assume(asset != address(0));
+
+        assetRegistry.addAsset(asset);
+
+        vm.expectEmit();
+        emit AssetRegistry.PauseAsset(asset);
+        assetRegistry.pauseAsset(asset);
+
+        (bool enabled, bool paused) = assetRegistry.getAssetStatus(asset);
+        assertTrue(enabled);
+        assertTrue(paused);
+    }
+}
