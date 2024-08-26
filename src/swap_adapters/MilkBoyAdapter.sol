afa78ab030578ae68e60718d282692f80ebfacb0
Jongseung Lim <penandlim@gmail.com>
2024-08-26 17:38:32 -0400

    chore: remove old adapter

--- src/swap_adapters/MilkBoyAdapter.sol
+++ src/swap_adapters/MilkBoyAdapter.sol
@@ -45,44 +45,61 @@ contract MilkBoyAdapter is TokenSwapAdapter {
         uint256 validTo = block.timestamp + 15 minutes;
         // TODO: emit events for each trade
         for (uint256 i = 0; i < externalTrades.length; i++) {
-            // TODO: use better salt
-            bytes32 salt = keccak256(abi.encode(externalTrades[i]));
-            address swapContract = ClonesWithImmutableArgs.addressOfClone3(salt);
-            // Create the order with the receiver being the cloned contract
-            GPv2Order.Data memory order = GPv2Order.Data({
-                sellToken: IERC20(externalTrades[i].sellToken),
-                buyToken: IERC20(externalTrades[i].buyToken),
-                receiver: swapContract,
-                sellAmount: externalTrades[i].sellAmount,
-                buyAmount: externalTrades[i].minAmount,
-                validTo: uint32(validTo),
-                appData: 0,
-                feeAmount: 0,
-                kind: GPv2Order.KIND_SELL,
-                partiallyFillable: false,
-                sellTokenBalance: GPv2Order.BALANCE_ERC20,
-                buyTokenBalance: GPv2Order.BALANCE_ERC20
-            });
-            ClonesWithImmutableArgs.clone3(
-                milkboyImplementation,
-                abi.encodePacked(
-                    order.hash(_DOMAIN_SEPARATOR),
-                    order.sellToken,
-                    order.buyToken,
-                    order.sellAmount,
-                    order.buyAmount,
-                    address(this),
-                    address(this)
-                ),
-                salt
+            _createOrder(
+                externalTrades[i].sellToken,
+                externalTrades[i].buyToken,
+                externalTrades[i].sellAmount,
+                externalTrades[i].minAmount,
+                validTo
             );
-            IERC20(externalTrades[i].sellToken).transfer(swapContract, externalTrades[i].sellAmount);
-            MilkBoy(swapContract).initialize();
         }
     }
 
     function isValidSignature(bytes32, bytes calldata) external pure override returns (bytes4) {
         revert ERC1271NotImplemented();
+    }
+
+    function _createOrder(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint256 validTo
+    )
+        internal
+    {
+        // Create the order with the receiver being the cloned contract
+        bytes32 salt = keccak256(abi.encode(sellToken, buyToken, sellAmount, buyAmount, validTo));
+        address swapContract = ClonesWithImmutableArgs.addressOfClone3(salt);
+        GPv2Order.Data memory order = GPv2Order.Data({
+            sellToken: IERC20(sellToken),
+            buyToken: IERC20(buyToken),
+            receiver: swapContract,
+            sellAmount: sellAmount,
+            buyAmount: buyAmount,
+            validTo: uint32(validTo),
+            appData: 0,
+            feeAmount: 0,
+            kind: GPv2Order.KIND_SELL,
+            partiallyFillable: false,
+            sellTokenBalance: GPv2Order.BALANCE_ERC20,
+            buyTokenBalance: GPv2Order.BALANCE_ERC20
+        });
+        ClonesWithImmutableArgs.clone3(
+            milkboyImplementation,
+            abi.encodePacked(
+                order.hash(_DOMAIN_SEPARATOR),
+                order.sellToken,
+                order.buyToken,
+                order.sellAmount,
+                order.buyAmount,
+                address(this),
+                address(this)
+            ),
+            salt
+        );
+        IERC20(sellToken).transfer(swapContract, sellAmount);
+        MilkBoy(swapContract).initialize();
     }
 
     function _milkBoyAdapterStorage() internal pure returns (MilkBoyAdapterStorage storage s) {
