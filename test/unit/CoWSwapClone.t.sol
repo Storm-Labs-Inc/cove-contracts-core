a685b79a05cca8f2a4cd566864a610833360f5dc
Jongseung Lim <penandlim@gmail.com>
2024-08-30 13:46:20 -0400

    test: increase coverage branches

--- test/unit/CoWSwapClone.t.sol
+++ test/unit/CoWSwapClone.t.sol
@@ -17,6 +17,7 @@ contract CoWSwapCloneTest is Test {
         0xc078f884a2676e1345748b1feace7b0abee5d00ecadb6e574dcdd109a63e8943;
 
     bytes4 internal constant _ERC1271_MAGIC_VALUE = 0x1626ba7e;
+    bytes4 internal constant _ERC1271_NON_MAGIC_VALUE = 0xffffffff;
 
     function setUp() public {
         // Deploy the CoWSwapClone implementation
@@ -73,7 +74,7 @@ contract CoWSwapCloneTest is Test {
     function testFuzz_initialize(
         address buyToken,
         uint256 sellAmount,
-        uint256 buyAmount,
+        uint256 minBuyAmount,
         uint32 validTo,
         address receiver,
         address operator,
@@ -82,7 +83,7 @@ contract CoWSwapCloneTest is Test {
         public
     {
         address sellToken = address(new ERC20Mock());
-        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, minBuyAmount, validTo, receiver, operator, salt);
         uint256 allowanceBefore = IERC20(sellToken).allowance(address(clone), _VAULT_RELAYER);
         assertEq(allowanceBefore, 0, "Allowance should be 0 before initialization");
         vm.expectCall(
@@ -97,6 +98,238 @@ contract CoWSwapCloneTest is Test {
         address sellToken,
         address buyToken,
         uint256 sellAmount,
+        uint256 minBuyAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt
+    )
+        public
+    {
+        vm.assume(buyAmount >= minBuyAmount);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, minBuyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        bytes32 orderDigest = order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(orderDigest, abi.encode(order)),
+            _ERC1271_MAGIC_VALUE,
+            "Invalid signature magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_OrderDigestIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        bytes32 badOrderDigest
+    )
+        public
+    {
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        vm.assume(badOrderDigest != order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR));
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(badOrderDigest, abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_SellTokenIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        address badSellToken
+    )
+        public
+    {
+        vm.assume(sellToken != badSellToken);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(badSellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_BuyTokenIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        address badBuyToken
+    )
+        public
+    {
+        vm.assume(buyToken != badBuyToken);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, badBuyToken, sellAmount, buyAmount, validTo, clone);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_SellAmountIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        uint256 badSellAmount
+    )
+        public
+    {
+        vm.assume(sellAmount != badSellAmount);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, badSellAmount, buyAmount, validTo, clone);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_BuyAmountIsLessThanExpected(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        uint256 badBuyAmount
+    )
+        public
+    {
+        vm.assume(badBuyAmount < buyAmount);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, badBuyAmount, validTo, clone);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_ValidToIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        uint32 badValidTo
+    )
+        public
+    {
+        vm.assume(validTo != badValidTo);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, badValidTo, clone);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_FeeAmountIsNotZero(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        uint256 feeAmount
+    )
+        public
+    {
+        vm.assume(feeAmount != 0);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        order.feeAmount = feeAmount;
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_KindIsNotSell(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        bytes1 kind
+    )
+        public
+    {
+        vm.assume(kind != GPv2Order.KIND_SELL);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = GPv2Order.Data({
+            sellToken: IERC20(sellToken),
+            buyToken: IERC20(buyToken),
+            receiver: receiver,
+            sellAmount: sellAmount,
+            buyAmount: buyAmount,
+            validTo: validTo,
+            appData: 0,
+            feeAmount: 0,
+            kind: kind,
+            partiallyFillable: false,
+            sellTokenBalance: GPv2Order.BALANCE_ERC20,
+            buyTokenBalance: GPv2Order.BALANCE_ERC20
+        });
+        bytes32 orderDigest = order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(orderDigest, abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_OrderIsPartiallyFillable(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
         uint256 buyAmount,
         uint32 validTo,
         address receiver,
@@ -106,25 +339,89 @@ contract CoWSwapCloneTest is Test {
         public
     {
         address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
-        GPv2Order.Data memory order = GPv2Order.Data({
-            sellToken: IERC20(sellToken),
-            buyToken: IERC20(buyToken),
-            receiver: clone,
-            sellAmount: sellAmount,
-            buyAmount: buyAmount,
-            validTo: uint32(validTo),
-            appData: 0,
-            feeAmount: 0,
-            kind: GPv2Order.KIND_SELL,
-            partiallyFillable: false,
-            sellTokenBalance: GPv2Order.BALANCE_ERC20,
-            buyTokenBalance: GPv2Order.BALANCE_ERC20
-        });
-        bytes32 orderDigest = order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        order.partiallyFillable = true;
 
-        bytes memory encodedOrder = abi.encode(order);
-        bytes4 result = CoWSwapClone(clone).isValidSignature(orderDigest, encodedOrder);
-        assertEq(result, _ERC1271_MAGIC_VALUE, "Invalid signature magic value");
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_SellTokenBalanceIsNotERC20(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        bytes32 badSellTokenBalance
+    )
+        public
+    {
+        vm.assume(badSellTokenBalance != GPv2Order.BALANCE_ERC20);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        order.sellTokenBalance = badSellTokenBalance;
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_BuyTokenBalanceIsNotERC20(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        bytes32 badBuyTokenBalance
+    )
+        public
+    {
+        vm.assume(badBuyTokenBalance != GPv2Order.BALANCE_ERC20);
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, clone);
+        order.buyTokenBalance = badBuyTokenBalance;
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
+    }
+
+    function testFuzz_isValidSignature_returnsNonMagicValueWhen_ReceiverIsNotCorrect(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver,
+        address operator,
+        bytes32 salt,
+        address badReceiver
+    )
+        public
+    {
+        address clone = testFuzz_clone(sellToken, buyToken, sellAmount, buyAmount, validTo, receiver, operator, salt);
+        vm.assume(badReceiver != clone);
+        GPv2Order.Data memory order = _getOrderData(sellToken, buyToken, sellAmount, buyAmount, validTo, badReceiver);
+
+        assertEq(
+            CoWSwapClone(clone).isValidSignature(order.hash(_COW_SETTLEMENT_DOMAIN_SEPARATOR), abi.encode(order)),
+            _ERC1271_NON_MAGIC_VALUE,
+            "Invalid signature non-magic value"
+        );
     }
 
     function testFuzz_claim(
@@ -182,6 +479,35 @@ contract CoWSwapCloneTest is Test {
         // Test that the claim function reverts if called by someone other than the operator or receiver
         vm.assume(caller != operator && caller != receiver);
         vm.expectRevert(CoWSwapClone.CallerIsNotOperatorOrReceiver.selector);
+        vm.prank(caller);
         CoWSwapClone(clone).claim();
+    }
+
+    function _getOrderData(
+        address sellToken,
+        address buyToken,
+        uint256 sellAmount,
+        uint256 buyAmount,
+        uint32 validTo,
+        address receiver
+    )
+        internal
+        pure
+        returns (GPv2Order.Data memory)
+    {
+        return GPv2Order.Data({
+            sellToken: IERC20(sellToken),
+            buyToken: IERC20(buyToken),
+            receiver: receiver,
+            sellAmount: sellAmount,
+            buyAmount: buyAmount,
+            validTo: validTo,
+            appData: 0,
+            feeAmount: 0,
+            kind: GPv2Order.KIND_SELL,
+            partiallyFillable: false,
+            sellTokenBalance: GPv2Order.BALANCE_ERC20,
+            buyTokenBalance: GPv2Order.BALANCE_ERC20
+        });
     }
 }
