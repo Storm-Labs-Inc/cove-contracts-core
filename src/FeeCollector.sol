646037422ca3f381dcdfdbb9085dbfc25b1bbb1d
Jongseung Lim <penandlim@gmail.com>
2024-10-28 16:59:46 -0400

    chore: update solidity and deps

--- src/FeeCollector.sol
+++ src/FeeCollector.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.23;
+pragma solidity 0.8.28;
 
 import { AccessControlEnumerable } from "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
 import { FixedPointMathLib } from "@solady/utils/FixedPointMathLib.sol";
