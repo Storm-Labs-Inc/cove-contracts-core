f5dfc5f26f567107e8fd468887330fa4eb4db280
Sunil Srivatsa <sunilsrivatsa@gmail.com>
2024-11-12 17:39:36 -0500

    fix: last newline

--- src/swap_adapters/CoWSwapClone.sol
+++ src/swap_adapters/CoWSwapClone.sol
@@ -5,7 +5,6 @@ import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Clone } from "clones-with-immutable-args/Clone.sol";
-
 import { GPv2Order } from "src/deps/cowprotocol/GPv2Order.sol";
 
 // slither-disable-start locked-ether
