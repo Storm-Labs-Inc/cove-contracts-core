40b11f687dedac6f39f993fe601bd835635fe548
Jongseung (John) Lim <penandlim@gmail.com>
2024-11-25 15:23:14 -0500

    test: improve testing bounds for AnchoredOracle (#127)
    * test: improve testing bounds for AnchoredOracle
    
    * chore: remove unused import

--- test/unit/AnchoredOracle.t.sol
+++ test/unit/AnchoredOracle.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.28;
 
 import { FixedPointMathLib } from "@solady/utils/FixedPointMathLib.sol";
-
 import { Errors } from "euler-price-oracle/src/lib/Errors.sol";
 
 import { BaseTest } from "test/utils/BaseTest.t.sol";
@@ -70,68 +69,80 @@ contract AnchoredOracleTest is BaseTest {
 
     function testFuzz_getQuote_matches(uint256 inAmount, address base, address quote, uint256 price) public {
         // bound to prevent overflow in MockPriceOracle
-        inAmount = bound(inAmount, 1, type(uint128).max);
-        price = bound(price, 1, type(uint128).max);
+        inAmount = bound(inAmount, 0, type(uint128).max);
+        price = bound(price, 0, type(uint128).max);
 
         primary.setPrice(base, quote, price);
         anchor.setPrice(base, quote, price);
 
         uint256 outAmount = oracle.getQuote(inAmount, base, quote);
-        assertEq(FixedPointMathLib.fullMulDivUp(inAmount, price, 1e18), outAmount);
+        assertEq(outAmount, primary.getQuote(inAmount, base, quote), "returns primary quote");
     }
 
     function testFuzz_getQuote_withinThreshold(uint256 inAmount, address base, address quote, uint256 price) public {
         // bound to prevent overflow in MockPriceOracle
-        // TODO: what is the lowest value we can use as a bounds?
-        inAmount = bound(inAmount, 1e18, type(uint128).max);
-        price = bound(price, 1e18, type(uint128).max);
+        inAmount = bound(inAmount, 0, type(uint128).max);
+        price = bound(price, 0, type(uint128).max);
         primary.setPrice(base, quote, price);
         uint256 primaryOut = primary.getQuote(inAmount, base, quote);
 
         // check the lower bound, rounding up
         uint256 lowerBound = FixedPointMathLib.fullMulDivUp(primaryOut, 1e18 - MAX_DIVERGENCE, 1e18);
-        uint256 anchorPrice = FixedPointMathLib.fullMulDivUp(lowerBound, 1e18, inAmount);
 
         // set the anchor price such that the quote output is equal to the lower bound
-        anchor.setPrice(base, quote, anchorPrice);
+        vm.mockCall(
+            address(anchor), abi.encodeCall(MockPriceOracle.getQuote, (inAmount, base, quote)), abi.encode(lowerBound)
+        );
         uint256 outAmount = oracle.getQuote(inAmount, base, quote);
-        assertEq(FixedPointMathLib.fullMulDivUp(inAmount, price, 1e18), outAmount);
+        assertEq(outAmount, primaryOut, "returns primary quote");
 
         // check the upper bound, rounding down
         uint256 upperBound = FixedPointMathLib.fullMulDiv(primaryOut, 1e18 + MAX_DIVERGENCE, 1e18);
-        anchorPrice = FixedPointMathLib.fullMulDiv(upperBound, 1e18, inAmount);
 
         // set the anchor price such that the quote output is equal to the upper bound
-        anchor.setPrice(base, quote, anchorPrice);
+        vm.mockCall(
+            address(anchor), abi.encodeCall(MockPriceOracle.getQuote, (inAmount, base, quote)), abi.encode(upperBound)
+        );
         outAmount = oracle.getQuote(inAmount, base, quote);
-        assertEq(FixedPointMathLib.fullMulDivUp(inAmount, price, 1e18), outAmount);
+        assertEq(outAmount, primaryOut, "returns primary quote");
     }
 
-    function testFuzz_getQuote_exceedsThreshold(uint256 inAmount, address base, address quote, uint256 price) public {
+    function testFuzz_getQuote_revertWhen_exceedsThreshold(
+        uint256 inAmount,
+        address base,
+        address quote,
+        uint256 price
+    )
+        public
+    {
         // bound to prevent overflow in MockPriceOracle
-        // TODO: what is the lowest value we can use as a bounds?
-        inAmount = bound(inAmount, 1e18, type(uint128).max);
-        price = bound(price, 1e18, type(uint128).max);
+        inAmount = bound(inAmount, 1, type(uint128).max);
+        price = bound(price, 1, type(uint128).max);
 
-        // set the primary price normally
         primary.setPrice(base, quote, price);
         uint256 primaryOut = primary.getQuote(inAmount, base, quote);
 
-        // calculate bounds
+        // check the lower bound, rounding up
         uint256 lowerBound = FixedPointMathLib.fullMulDivUp(primaryOut, 1e18 - MAX_DIVERGENCE, 1e18);
-        uint256 anchorPrice = FixedPointMathLib.fullMulDivUp(lowerBound, 1e18, inAmount) - 1;
 
         // set the anchor price such that the quote output is 1 less than the lower bound
-        anchor.setPrice(base, quote, anchorPrice - 1);
+        vm.mockCall(
+            address(anchor),
+            abi.encodeCall(MockPriceOracle.getQuote, (inAmount, base, quote)),
+            abi.encode(lowerBound - 1)
+        );
         vm.expectRevert(Errors.PriceOracle_InvalidAnswer.selector);
         oracle.getQuote(inAmount, base, quote);
 
-        // calculate bounds
+        // check the upper bound, rounding down
         uint256 upperBound = FixedPointMathLib.fullMulDiv(primaryOut, 1e18 + MAX_DIVERGENCE, 1e18);
-        anchorPrice = FixedPointMathLib.fullMulDiv(upperBound, 1e18, inAmount) + 1;
 
         // set the anchor price such that the quote output is 1 more than the upper bound
-        anchor.setPrice(base, quote, anchorPrice);
+        vm.mockCall(
+            address(anchor),
+            abi.encodeCall(MockPriceOracle.getQuote, (inAmount, base, quote)),
+            abi.encode(upperBound + 1)
+        );
         vm.expectRevert(Errors.PriceOracle_InvalidAnswer.selector);
         oracle.getQuote(inAmount, base, quote);
     }
