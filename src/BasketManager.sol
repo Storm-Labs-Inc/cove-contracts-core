9428869bca5733663da46b2d09d5cdf15da83e76
lilPlumberBoy <dalymike102@gmail.com>
2025-01-28 12:54:58 -0500

    fix: updated natspec comments

--- src/BasketManager.sol
+++ src/BasketManager.sol
@@ -585,7 +585,8 @@ contract BasketManager is ReentrancyGuardTransient, AccessControlEnumerable, Pau
     }
 
     /// @notice Allows the timelock to execute an arbitrary function call on a target contract.
-    /// @dev Can only be called by addresses with the timelock role. Reverts if the execution fails.
+    /// @dev Can only be called by addresses with the timelock role. Reverts if the execution fails. Reverts if the
+    /// target of the call is an asset that is active in the asset registry.
     /// @param target The address of the target contract.
     /// @param data The calldata to send to the target contract.
     /// @param value The amount of Ether (in wei) to send with the call.
