0d72ad4600798f59b9f211508638d0dea1acdd5f
lilPlumberBoy <dalymike102@gmail.com>
2025-02-26 23:09:24 -0500

    feat: added farming and fallback states

--- test/forked/GenerateStatesForFrontend.t.sol
+++ test/forked/GenerateStatesForFrontend.t.sol
@@ -4,14 +4,19 @@ pragma solidity 0.8.28;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { EulerRouter } from "euler-price-oracle/src/EulerRouter.sol";
 import { Vm } from "forge-std/Vm.sol";
 import { console } from "forge-std/console.sol";
+import { ERC20Mock } from "test/utils/mocks/ERC20Mock.sol";
 
+import { FarmingPlugin } from "@1inch/farming/contracts/FarmingPlugin.sol";
 import { BasketManager } from "src/BasketManager.sol";
 import { BasketToken } from "src/BasketToken.sol";
+
 import { IMasterRegistry } from "src/interfaces/IMasterRegistry.sol";
+import { ManagedWeightStrategy } from "src/strategies/ManagedWeightStrategy.sol";
 import { BasketTradeOwnership, ExternalTrade, InternalTrade } from "src/types/Trades.sol";
 
 import { BaseTest } from "test/utils/BaseTest.t.sol";
@@ -21,6 +26,8 @@ contract GenerateStatesForFrontend is BaseTest {
     address public user = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address public basketManager;
     address public basketToken;
+    address public farmingPlugin;
+    address public weightStrategy;
     address public rebalanceProposer = COVE_SILVERBACK_AWS_ACCOUNT;
     address public tokenSwapProposer = COVE_SILVERBACK_AWS_ACCOUNT;
     address public tokenSwapExecutor = COVE_SILVERBACK_AWS_ACCOUNT;
@@ -32,6 +39,8 @@ contract GenerateStatesForFrontend is BaseTest {
         forkNetworkAt("mainnet", 21_928_744);
         basketManager = _getFromStagingMasterRegistry("BasketManager");
         basketToken = BasketManager(basketManager).basketTokens()[0];
+        weightStrategy = BasketToken(basketToken).strategy();
+        farmingPlugin = _getFromStagingMasterRegistry("Staging_Stables_FarmingPlugin");
         super.setUp();
         labelKnownAddresses();
 
@@ -45,15 +54,27 @@ contract GenerateStatesForFrontend is BaseTest {
         // Fast forward to 1 day after the fork to ensure rebalance is possible
         vm.warp(vm.getBlockTimestamp() + 1 days);
 
+        // Setup farming plugin with rewards
+        uint256 rewardAmount = 100e18;
+        uint256 rewardPeriod = 1 days;
+        ERC20Mock farmingRewardToken = ERC20Mock(address(FarmingPlugin(farmingPlugin).REWARDS_TOKEN()));
+        farmingRewardToken.mint(COVE_DEPLOYER_ADDRESS, rewardAmount);
+        vm.startPrank(COVE_DEPLOYER_ADDRESS);
+        farmingRewardToken.approve(farmingPlugin, rewardAmount);
+        FarmingPlugin(farmingPlugin).setDistributor(COVE_DEPLOYER_ADDRESS);
+        FarmingPlugin(farmingPlugin).startFarming(rewardAmount, rewardPeriod);
+        vm.stopPrank();
+
         // 1. Give USDC to user
         deal(ETH_USDC, user, AIRDROP_AMOUNT * _getOneUnit(ETH_USDC));
         _dumpStateWithTimestamp("01_accountHasSomeUSDC");
 
-        // 2. Request deposit USDC into basket
+        // 2. Request deposit USDC into basket, add farming plugin
         vm.startPrank(user);
         uint256 depositAmount = DEPOSIT_AMOUNT * _getOneUnit(ETH_USDC);
         IERC20(ETH_USDC).approve(basketToken, depositAmount);
         BasketToken(basketToken).requestDeposit(depositAmount, user, user);
+        BasketToken(basketToken).addPlugin(farmingPlugin);
         vm.stopPrank();
         _dumpStateWithTimestamp("02_accountHasRequestedDeposit");
 
@@ -66,9 +87,9 @@ contract GenerateStatesForFrontend is BaseTest {
         _dumpStateWithTimestamp("03_accountHasClaimableBasketTokenShares");
 
         // 4. Claim shares
-        uint256 balance = BasketToken(basketToken).maxDeposit(user);
+        uint256 assets = BasketToken(basketToken).maxDeposit(user);
         vm.prank(user);
-        BasketToken(basketToken).deposit(balance, user, user);
+        BasketToken(basketToken).deposit(assets, user, user);
         _dumpStateWithTimestamp("04_accountHasBasketTokenShares");
 
         // Generate external trades, execute them, mock cowswap activity, then complete rebalance
@@ -80,16 +101,43 @@ contract GenerateStatesForFrontend is BaseTest {
             _buildSingleExternalTrade(basketToken, ETH_USDC, ETH_SFRAX, depositAmount * targetWeights[0][2] / 1e18);
         externalTrades[2] =
             _buildSingleExternalTrade(basketToken, ETH_USDC, ETH_SUSDE, depositAmount * targetWeights[0][3] / 1e18);
-        // 5. Complete rebalance, account can now pro-rata redeem
+
+        // 5. Complete rebalance, account can now redeem
         _continueAndCompleteRebalance(basketTokens, externalTrades);
+        // check rebalance status
         _dumpStateWithTimestamp("05_protocolHasCompletedRebalance_accountCanProRataRedeem");
 
         // 6. Request Redeem
-        vm.prank(user);
-        BasketToken(basketToken).requestRedeem(balance, user, user);
+        vm.startPrank(user);
+        BasketToken(basketToken).requestRedeem(BasketToken(basketToken).balanceOf(user), user, user);
+        vm.stopPrank();
         _dumpStateWithTimestamp("06_accountHasRequestedRedeem");
 
         vm.warp(vm.getBlockTimestamp() + 1 days);
+
+        // 7. Claim rewards
+        vm.prank(user);
+        assertTrue(FarmingPlugin(farmingPlugin).farmed(user) > 0, "User has no rewards to claim");
+        _dumpStateWithTimestamp("07_accountHasClaimableRewards");
+
+        // Propose rebalance and attempt to complete rebalance until max retries is reached
+        // Trades are not proposed, cycle through complete and propose rebalance until max retries is reached
+        vm.startPrank(rebalanceProposer);
+        _refreshPriceFeeds();
+        BasketManager(basketManager).proposeRebalance(basketTokens);
+        // Complete rebalance until max retries is reached (3)
+        for (uint256 i = 0; i <= BasketManager(basketManager).retryLimit(); i++) {
+            vm.warp(vm.getBlockTimestamp() + 15 minutes);
+            _refreshPriceFeeds();
+            BasketManager(basketManager).completeRebalance(
+                externalTrades, basketTokens, _getBasketTagetWeights(basketTokens), _getBasketAssets(basketTokens)
+            );
+        }
+        vm.stopPrank();
+
+        // Check that fallback redeeem is now possible
+        assertTrue(BasketToken(basketToken).claimableFallbackShares(user) > 0, "FallbackShares not claimable");
+        _dumpStateWithTimestamp("08_accountHasClaimableFallbackShares");
     }
 
     function _getFromStagingMasterRegistry(bytes32 key) internal view returns (address) {
