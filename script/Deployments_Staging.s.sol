134f1f6ba5101d8e6b68994883466aae53e77817
Jongseung (John) Lim <penandlim@gmail.com>
2025-03-11 22:16:46 -0400

    feat: add deployment scripts for different environments (#236)
    * feat: add deployment scripts for different environments
    
    - Refactored Deployments.s.sol to be an abstract base contract
    - Added Deployments_Staging.s.sol for staging environment deployment
    - Added Deployments_Production.s.sol for production environment deployment
    - Added Deployments_Test.s.sol for integration test deployment
    - Updated package.json to use Deployments_Staging.s.sol for local deployment
    - Introduced abstract methods for environment-specific configurations
    - Improved modularity and flexibility of deployment scripts
    
    * chore: remove unusued env

---
+++ script/Deployments_Staging.s.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.23;
+
+import { BasketTokenDeployment, Deployments, OracleOptions } from "./Deployments.s.sol";
+import { CustomDeployerFunctions } from "./utils/CustomDeployerFunctions.sol";
+import { Deployer, DeployerFunctions } from "generated/deployer/DeployerFunctions.g.sol";
+import { ERC20Mock } from "test/utils/mocks/ERC20Mock.sol";
+
+// solhint-disable contract-name-camelcase
+contract Deployments_Staging is Deployments {
+    using DeployerFunctions for Deployer;
+    using CustomDeployerFunctions for Deployer;
+
+    function _buildPrefix() internal pure override returns (string memory) {
+        return "Staging_";
+    }
+
+    function _setPermissionedAddresses() internal virtual override {
+        // Set permissioned addresses
+        // Staging deploy
+        admin = COVE_STAGING_COMMUNITY_MULTISIG;
+        treasury = COVE_STAGING_COMMUNITY_MULTISIG;
+        pauser = COVE_DEPLOYER_ADDRESS;
+        manager = COVE_STAGING_OPS_MULTISIG;
+        timelock = getAddress(buildTimelockControllerName());
+        rebalanceProposer = STAGING_COVE_SILVERBACK_AWS_ACCOUNT;
+        tokenSwapProposer = STAGING_COVE_SILVERBACK_AWS_ACCOUNT;
+        tokenSwapExecutor = STAGING_COVE_SILVERBACK_AWS_ACCOUNT;
+    }
+
+    function _deployNonCoreContracts() internal override {
+        address[] memory basketAssets = new address[](4);
+        basketAssets[0] = ETH_USDC;
+        basketAssets[1] = ETH_SDAI;
+        basketAssets[2] = ETH_SUSDE;
+        basketAssets[3] = ETH_SFRXUSD;
+
+        // 0. USDC
+        _deployDefaultAnchoredOracleForAsset(
+            ETH_USDC,
+            OracleOptions({
+                pythPriceFeed: PYTH_USDC_USD_FEED,
+                pythMaxStaleness: 30 seconds,
+                pythMaxConfWidth: 50, //0.5%
+                chainlinkPriceFeed: ETH_CHAINLINK_USDC_USD_FEED,
+                chainlinkMaxStaleness: 1 days,
+                maxDivergence: 0.005e18 // 0.5%
+             })
+        );
+        _addAssetToAssetRegistry(ETH_USDC);
+
+        // 1. sDAI
+        _deployDefaultAnchoredOracleForAsset(
+            ETH_DAI,
+            OracleOptions({
+                pythPriceFeed: PYTH_DAI_USD_FEED,
+                pythMaxStaleness: 30 seconds,
+                pythMaxConfWidth: 50, //0.5%
+                chainlinkPriceFeed: ETH_CHAINLINK_DAI_USD_FEED,
+                chainlinkMaxStaleness: 1 days,
+                maxDivergence: 0.005e18 // 0.5%
+             })
+        );
+        _addAssetToAssetRegistry(ETH_SDAI);
+
+        // 2. sUSDe
+        _deployDefaultAnchoredOracleForAsset(
+            ETH_SUSDE,
+            OracleOptions({
+                pythPriceFeed: PYTH_USDE_USD_FEED,
+                pythMaxStaleness: 30 seconds,
+                pythMaxConfWidth: 50, //0.5%
+                chainlinkPriceFeed: ETH_CHAINLINK_USDE_USD_FEED,
+                chainlinkMaxStaleness: 1 days,
+                maxDivergence: 0.005e18 // 0.5%
+             })
+        );
+        _addAssetToAssetRegistry(ETH_SUSDE);
+
+        // 3. sfrxUSD/sUSDe -> USD
+        _deployCurveEMAOracleCrossAdapterForNonUSDPair(
+            ETH_SFRXUSD,
+            ETH_CURVE_SFRXUSD_SUSDE_POOL,
+            ETH_SUSDE,
+            OracleOptions({
+                pythPriceFeed: PYTH_SUSDE_USD_FEED,
+                pythMaxStaleness: 30 seconds,
+                pythMaxConfWidth: 50, //0.5%
+                chainlinkPriceFeed: ETH_CHAINLINK_SUSDE_USD_FEED,
+                chainlinkMaxStaleness: 1 days,
+                maxDivergence: 0.005e18 // 0.5%
+             }),
+            1 // SUSDE
+        );
+        _addAssetToAssetRegistry(ETH_SFRXUSD);
+
+        // Deploy launch strategy
+        _deployManagedStrategy(COVE_DEPLOYER_ADDRESS, "Gauntlet V1");
+
+        uint64[] memory initialWeights = new uint64[](4);
+        initialWeights[0] = 0;
+        initialWeights[1] = 0.7777777777777778e18;
+        initialWeights[2] = 0.1111111111111111e18;
+        initialWeights[3] = 0.1111111111111111e18;
+
+        _setInitialWeightsAndDeployBasketToken(
+            BasketTokenDeployment({
+                name: "Stables",
+                symbol: "stgUSD",
+                rootAsset: ETH_USDC,
+                bitFlag: assetsToBitFlag(basketAssets),
+                strategy: getAddress(buildManagedWeightStrategyName("Gauntlet V1")),
+                initialWeights: initialWeights
+            })
+        );
+
+        // Deploy ERC20Mock for farming plugin rewards
+        ERC20Mock mockERC20 = deployer.deploy_ERC20Mock("CoveMockERC20");
+
+        // Deploy farming plugin
+        address basketToken = getAddress(buildBasketTokenName("Stables"));
+        address farmingPlugin = address(
+            deployer.deploy_FarmingPlugin(
+                buildFarmingPluginName(basketToken, address(mockERC20)),
+                basketToken,
+                address(mockERC20),
+                COVE_DEPLOYER_ADDRESS
+            )
+        );
+        _addToMasterRegistryLater("Stables_FarmingPlugin", farmingPlugin);
+    }
+}
