97d97fbfe420500fffd4112f49bbf5b26a275df7
Jongseung (John) Lim <penandlim@gmail.com>
2025-05-13 11:03:07 -0400

    chore: remove test account PK

--- test/forked/GenerateStatesForFrontend.t.sol
+++ test/forked/GenerateStatesForFrontend.t.sol
@@ -24,7 +24,6 @@ import { ERC20Mock } from "test/utils/mocks/ERC20Mock.sol";
 contract GenerateStatesForFrontend is BaseTest {
     // Account used for testing
     address public user = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-    uint256 public userPK = uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
     address public basketManager;
     address public basketToken;
     address public farmingPlugin;
